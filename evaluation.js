c648ee2175dfc92854ad25a3034f5646c318e068
solankiamn8 <solankiamn8@gmail.com>
2025-03-09 12:57:11 +0530

    update evaluation.js

--- evaluation.js
+++ evaluation.js
@@ -3,26 +3,32 @@ function taskManagementSystem(){
 
     return {
         addTask: function(title){
-            return tasks.push({title, status: "pending"});;
+            tasks.push({title, status: "pending"});
+            return this
         },
         completeTask: function(title){
-            return tasks = tasks.map(task =>
-                task.title == title ? {...task, stats: "completed"}: task
+            tasks = tasks.map(task =>
+                task.title == title ? {...task, status: "completed"}: task
             )
+            return this
         },
         removeTask: function(title){
-            return tasks = tasks.filter(task => task.title !== title)
+            tasks = tasks.filter(task => task.title !== title)
+            return this
         },
         listTask: function(){
-            return tasks.forEach(task => console.log(task))
+            tasks.forEach(task => console.log(task))
+            return this
         },
         filterStatus: function(status){
-            return console.log(tasks.filter(task => task.status == status))
+            console.log(tasks.filter(task => task.status == status))
+            return this
         },
         countTask: function(){
             let completedCount = tasks.filter(task => task.status == "completed").length
             let pendingCount = tasks.filter(task => task.status == "pending").length
             console.log(`Completed: ${completedCount}, Pending: ${pendingCount}`)
+            return this
         },
         getTask: function(){
             return tasks
@@ -33,7 +39,6 @@ function taskManagementSystem(){
 
 const run = taskManagementSystem()
 
-run.addTask("t1").listTask()
-// run.addTask("task1").addTask("task2").completeTask("task1")
-//     .addTask("task3").removeTask("task1").listTask()
-//     .filterStatus("pending").countTask().getTasks()
+run.addTask("task1").addTask("task2").completeTask("task3")
+    .addTask("task3").removeTask("task1").listTask()
+    .filterStatus("pending").countTask().listTask()
